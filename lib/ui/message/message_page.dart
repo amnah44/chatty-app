c39b12fbeb5de60218694754c6cbbf623f1ebab1
Amnah Ali <amnah1li@gmail.com>
2022-06-30 17:31:16 +0300

    add text direction to box of type the message

--- lib/ui/message/message_page.dart
+++ lib/ui/message/message_page.dart
@@ -1,4 +1,3 @@
-import 'package:auto_direction/auto_direction.dart';
 import 'package:flutter/material.dart';
 
 class MessagePage extends StatefulWidget {
@@ -11,6 +10,7 @@ class MessagePage extends StatefulWidget {
 }
 
 class _MessagePageState extends State<MessagePage> {
+  final ValueNotifier<TextDirection> _textDir = ValueNotifier(TextDirection.ltr);
   final _controller = TextEditingController();
   var _message;
 
@@ -19,6 +19,36 @@ class _MessagePageState extends State<MessagePage> {
     widget.onSubmit(_message);
     _controller.clear();
     _message = "";
+  }
+
+  TextDirection _getDirection(String v) {
+    final string = v.trim();
+    if (string.isEmpty) return TextDirection.ltr;
+    final firstUnit = string.codeUnitAt(0);
+    if (firstUnit > 0x0600 && firstUnit < 0x06FF ||
+        firstUnit > 0x0750 && firstUnit < 0x077F ||
+        firstUnit > 0x07C0 && firstUnit < 0x07EA ||
+        firstUnit > 0x0840 && firstUnit < 0x085B ||
+        firstUnit > 0x08A0 && firstUnit < 0x08B4 ||
+        firstUnit > 0x08E3 && firstUnit < 0x08FF ||
+        firstUnit > 0xFB50 && firstUnit < 0xFBB1 ||
+        firstUnit > 0xFBD3 && firstUnit < 0xFD3D ||
+        firstUnit > 0xFD50 && firstUnit < 0xFD8F ||
+        firstUnit > 0xFD92 && firstUnit < 0xFDC7 ||
+        firstUnit > 0xFDF0 && firstUnit < 0xFDFC ||
+        firstUnit > 0xFE70 && firstUnit < 0xFE74 ||
+        firstUnit > 0xFE76 && firstUnit < 0xFEFC ||
+        firstUnit > 0x10800 && firstUnit < 0x10805 ||
+        firstUnit > 0x1B000 && firstUnit < 0x1B0FF ||
+        firstUnit > 0x1D165 && firstUnit < 0x1D169 ||
+        firstUnit > 0x1D16D && firstUnit < 0x1D172 ||
+        firstUnit > 0x1D17B && firstUnit < 0x1D182 ||
+        firstUnit > 0x1D185 && firstUnit < 0x1D18B ||
+        firstUnit > 0x1D1AA && firstUnit < 0x1D1AD ||
+        firstUnit > 0x1D242 && firstUnit < 0x1D244) {
+      return TextDirection.rtl;
+    }
+    return TextDirection.ltr;
   }
 
   @override
@@ -35,30 +65,41 @@ class _MessagePageState extends State<MessagePage> {
       child: Row(
         children: [
           Expanded(
-            child: TextField(
-              minLines: 1,
-              maxLines: 20,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                fontFamily: 'Montserrat',
-              ),
-              decoration: InputDecoration(
-                hintText: "Type a message",
-                contentPadding: const EdgeInsets.all(8),
-                fillColor: Colors.white,
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              controller: _controller,
-              onChanged: (value) {
-                setState(() {
-                  _message = value;
-                });
-              },
+            child: ValueListenableBuilder<TextDirection>(
+              valueListenable: _textDir,
+              builder: (context, value, child) =>
+                  TextField(
+                    minLines: 1,
+                    maxLines: 20,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Montserrat',
+                    ),
+                    decoration: InputDecoration(
+                      hintText: "Type a message",
+                      contentPadding: const EdgeInsets.all(8),
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    textDirection: value,
+                    controller: _controller,
+                    onChanged: (input) {
+                      setState(() {
+                        _message = input;
+                        if(_message.trim().length < 2){
+                          final dir = _getDirection(_message);
+                          if(dir != value) _textDir.value = dir;
+                        }
+
+                      });
+                    },
+                  )
+              ,
             ),
           ),
           const SizedBox(width: 4),
@@ -84,3 +125,7 @@ class _MessagePageState extends State<MessagePage> {
     );
   }
 }
+
+/*
+
+ */
